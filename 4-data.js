ad634068164bd2dc3ef0f054f50bd70b11994c99
INTEGRA-DESEMPENHO <missioleo620@gmail.com>
2026-05-27 17:45:49 -0300

    4-data.js

---
+++ 4-data.js
@@ -0,0 +1,8 @@
+// data.js — Edite aqui sua base de dados
+window.BASE_DE_DADOS = [
+  ['Ordem','Unidade','UniNorm','Bloco','Pavimento','AmbTag','NomeAmb','Verificação','Aval','Adeq','Inad','Pend','Obs','UID'],
+  ['1','REI PELE','rei pele','Bloco A','1º Andar','AMB01','Recepção','Verificar piso','Não','FALSO','FALSO','','','rei pele||bloco a||1º andar||recepção||verificar piso'],
+  ['2','REI PELE','rei pele','Bloco A','1º Andar','AMB01','Recepção','Verificar iluminação','Não','FALSO','FALSO','','','rei pele||bloco a||1º andar||recepção||verificar iluminação'],
+  ['3','REI PELE','rei pele','Bloco A','2º Andar','AMB02','Corredor','Verificar corrimão','Não','FALSO','FALSO','','','rei pele||bloco a||2º andar||corredor||verificar corrimão'],
+  ['4','PAPA FRANCISCO','papa francisco','Bloco B','Térreo','AMB03','Hall','Verificar extintores','Não','FALSO','FALSO','','','papa francisco||bloco b||térreo||hall||verificar extintores']
+];
