62dee1e4d841ddc1c090d982a31c72db3e0b8bd0
INTEGRA-DESEMPENHO <missioleo620@gmail.com>
2026-05-28 16:34:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 ═══════════════════════════════════════════════════════ */
 
 // ─── CONFIGURAÇÃO — TROQUE PELA SUA URL DO GAS ───────────
-var GAS_URL = 'https://sheetdb.io/api/v1/p5kdwbijb335u'; 
+var GAS_URL = 'https://script.google.com/macros/s/AKfycbyS8urqPKwu8l6UnpyysKy5mweOH_u0zYKbpJajxXfQkdNeHSkev_EedbWDYCoylvlF/exec'; 
 // Exemplo: 'https://script.google.com/macros/s/AKfycbXXXXXXXXXXXX/exec'
 
 // ─── Chaves localStorage ─────────────────────────────────
